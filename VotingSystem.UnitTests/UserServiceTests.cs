9b2f1271524fb1b12bfc9c0ed4c7e8f7e1bd6651
kkcaZ <z_jagger@hotmail.co.uk>
2023-02-12 21:55:00 +0000

    Added Tests

---
+++ VotingSystem.UnitTests/UserServiceTests.cs
@@ -0,0 +1,15 @@
+namespace VotingSystem.UnitTests;
+
+public class UserServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
