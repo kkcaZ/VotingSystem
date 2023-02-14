059fcd338788c3397175533e7a6313b4443e436c
kkcaZ <z_jagger@hotmail.co.uk>
2023-02-14 20:47:06 +0000

    Candidates Stored on Invite Accept

--- VotingSystem/Services/Abstraction/IElectionService.cs
+++ VotingSystem/Services/Abstraction/IElectionService.cs
@@ -9,4 +9,6 @@ public interface IElectionService
     public bool DeleteElection(Guid electionId);
     public bool UpdateElection(Election election, List<ElectionInviteUpdate> inviteUpdates);
     public List<ElectionInviteModel> GetElectionCandidateInvites(Guid electionId);
+    public bool AddCandidate(Guid userId, Guid electionId);
+    public bool DeleteCandidate(Guid electionId, Guid userId);
 }
